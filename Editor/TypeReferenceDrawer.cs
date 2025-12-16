0fadce9e1a00dfe517b5cfddd177c71f1e41bced
MehmetSelmanSavas <mehmetselmansavas@gmail.com>
2025-12-16 21:40:51 +0300

    Upgraded type reference drawer for better and faster performance

--- Editor/TypeReferenceDrawer.cs
+++ Editor/TypeReferenceDrawer.cs
@@ -1,119 +1,134 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
 using System.Linq;
 using System.Reflection;
+using UnityEditor;
+using UnityEngine;
 
-//Disabled due to not finding the correct assembly for the type we cant. Will need a rework here :/
-//[CustomPropertyDrawer(typeof(TypeReference), true)]
-//[CustomPropertyDrawer(typeof(TypeReferenceOptionsAttribute))]
-public class TypeReferenceDrawer : PropertyDrawer
+namespace GD.Utilities.Editor
 {
-    string[] _foundTypeNames = new string[0];
-    System.Type[] _foundTypes = new Type[0];
-
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    //Disabled due to not finding the correct assembly for the type we cant. Will need a rework here :/
+    [CustomPropertyDrawer(typeof(TypeReference), true)]
+    [CustomPropertyDrawer(typeof(TypeReferenceOptionsAttribute))]
+    public class TypeReferenceDrawer : PropertyDrawer
     {
-        int index = 0;
-        int selectedIndex = 0;
+        string[] _foundTypeNames = new string[0];
+        System.Type[] _foundTypes = new Type[0];
 
-        TypeReference typeReference = Utils.GetTargetObjectOfProperty(property) as TypeReference;
-        var typeOptionsAttribute = attribute as TypeReferenceOptionsAttribute ?? null;
+        List<Type> assemblyTypes = new();
 
-        if (_foundTypes.Length <= 0)
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            Assembly assembly = typeReference.Type != null ? Assembly.GetAssembly(typeReference.Type) : Assembly.GetAssembly(typeof(GameObject));
+            int index = 0;
+            int selectedIndex = 0;
 
-            List<Type> assemblyTypes = assembly.GetTypes().ToList();
+            TypeReference typeReference = TypeHelper.GetTargetObjectOfProperty(property) as TypeReference;
+            var typeOptionsAttribute = attribute as TypeReferenceOptionsAttribute ?? null;
 
-            if (typeOptionsAttribute != null)
+            if (_foundTypes.Length <= 0)
             {
-                if (typeOptionsAttribute.InheritedTypes.Count > 0)
-                    assemblyTypes.RemoveAll(x => !IsInheritedFrom(typeOptionsAttribute.InheritedTypes, x));
-
-                if (typeOptionsAttribute.IncludeTypes.Count > 0)
-                    assemblyTypes.RemoveAll(x => !IsContainedInIncluded(typeOptionsAttribute.IncludeTypes, x));
-
-                if (typeOptionsAttribute.RestrictTypes.Count > 0)
-                    assemblyTypes.RemoveAll(x => !typeOptionsAttribute.RestrictTypes.Contains(x));
-            }
-
-            if (Utils.GetTargetObjectOfProperty(property).GetType().IsGenericType)
-            {
-                object foundObj = Utils.GetTargetObjectOfProperty(property);
-                System.Type type = foundObj.GetType().GetGenericTypeDefinition();
-
-                if (type == typeof(TypeReferenceInheritedFrom<>).GetGenericTypeDefinition())
+                if (assemblyTypes.Count <= 0)
                 {
-                    assemblyTypes.RemoveAll(x => !IsInheritedFrom(foundObj.GetType().GetGenericArguments().ToList(), x));
+                    assemblyTypes = AppDomain.CurrentDomain.GetAssemblies()
+                                .SelectMany(assembly => assembly.GetTypes())
+                                .Where(t => DefaultFilter(t))
+                                .ToList();
                 }
-                else if (type == typeof(TypeReferenceIncludesFrom<>).GetGenericTypeDefinition())
+
+                if (typeOptionsAttribute != null)
                 {
-                    assemblyTypes.RemoveAll(x => !IsContainedInIncluded(foundObj.GetType().GetGenericArguments().ToList(), x));
+                    if (typeOptionsAttribute.InheritedTypes.Count > 0)
+                        assemblyTypes.RemoveAll(x => !IsInheritedFrom(typeOptionsAttribute.InheritedTypes, x));
+
+                    if (typeOptionsAttribute.IncludeTypes.Count > 0)
+                        assemblyTypes.RemoveAll(x => !IsContainedInIncluded(typeOptionsAttribute.IncludeTypes, x));
+
+                    if (typeOptionsAttribute.RestrictTypes.Count > 0)
+                        assemblyTypes.RemoveAll(x => !typeOptionsAttribute.RestrictTypes.Contains(x));
+                }
+
+                if (TypeHelper.GetTargetObjectOfProperty(property).GetType().IsGenericType)
+                {
+                    object foundObj = TypeHelper.GetTargetObjectOfProperty(property);
+                    System.Type type = foundObj.GetType().GetGenericTypeDefinition();
+
+                    if (type == typeof(TypeReferenceInheritedFrom<>).GetGenericTypeDefinition())
+                    {
+                        assemblyTypes.RemoveAll(x => !IsInheritedFrom(foundObj.GetType().GetGenericArguments().ToList(), x));
+                    }
+                    else if (type == typeof(TypeReferenceIncludesFrom<>).GetGenericTypeDefinition())
+                    {
+                        assemblyTypes.RemoveAll(x => !IsContainedInIncluded(foundObj.GetType().GetGenericArguments().ToList(), x));
+                    }
+                }
+
+                _foundTypes = assemblyTypes.ToArray();
+            }
+
+
+            if (_foundTypeNames.Length <= 0)
+            {
+                if ((typeOptionsAttribute?.ShouldSort).GetValueOrDefault())
+                    Array.Sort(_foundTypes, (x, y) => String.Compare(x.Name, y.Name));
+
+                _foundTypeNames = _foundTypes.Select(x => new String(x.Name)).ToArray();
+            }
+
+            index = Array.IndexOf(_foundTypeNames, typeReference.Type?.Name);
+
+            //EditorGUI.BeginProperty(position, label, property);
+            selectedIndex = EditorGUI.Popup(position, label.text, index, _foundTypeNames);
+            //EditorGUI.EndProperty();
+
+            if (selectedIndex >= 0 && index != selectedIndex)
+            {
+                typeReference.SetType(_foundTypes[selectedIndex]);
+                property.serializedObject.ApplyModifiedProperties();
+                property.serializedObject.UpdateIfRequiredOrScript();
+
+                EditorUtility.SetDirty(property.serializedObject.targetObject);
+
+                if (property.serializedObject.targetObject is ScriptableObject scriptableObject)
+                {
+                    AssetDatabase.SaveAssetIfDirty(scriptableObject);
                 }
             }
-
-            _foundTypes = assemblyTypes.ToArray();
         }
 
-
-        if (_foundTypeNames.Length <= 0)
+        static bool DefaultFilter(Type type)
         {
-            if ((typeOptionsAttribute?.ShouldSort).GetValueOrDefault())
-                Array.Sort(_foundTypes, (x, y) => String.Compare(x.Name, y.Name));
-
-            _foundTypeNames = _foundTypes.Select(x => new String(x.Name)).ToArray();
+            return !type.IsAbstract && !type.IsInterface && !type.IsGenericType;
         }
 
-        index = Array.IndexOf(_foundTypeNames, typeReference.Type?.Name);
-
-        //EditorGUI.BeginProperty(position, label, property);
-        selectedIndex = EditorGUI.Popup(position, label.text, index, _foundTypeNames);
-        //EditorGUI.EndProperty();
-
-        if (selectedIndex >= 0 && index != selectedIndex)
+        public bool IsInheritedFrom(List<Type> inheritedTypes, Type check)
         {
-            typeReference.SetType(_foundTypes[selectedIndex]);
-            property.serializedObject.ApplyModifiedProperties();
-            property.serializedObject.UpdateIfRequiredOrScript();
+            if (inheritedTypes.Count <= 0)
+                return true;
 
-            EditorUtility.SetDirty(property.serializedObject.targetObject);
+            if (check.IsAbstract)
+                return false;
 
-            if (property.serializedObject.targetObject is ScriptableObject scriptableObject)
+            for (int i = 0; i < inheritedTypes.Count; i++)
             {
-                AssetDatabase.SaveAssetIfDirty(scriptableObject);
+                if (check.IsSubclassOf(inheritedTypes[i]))
+                    return true;
             }
-        }
-    }
-
-    public bool IsInheritedFrom(List<Type> inheritedTypes, Type check)
-    {
-        if (inheritedTypes.Count <= 0)
-            return true;
-
-        if (check.IsAbstract)
             return false;
-
-        for (int i = 0; i < inheritedTypes.Count; i++)
-        {
-            if (check.IsSubclassOf(inheritedTypes[i]))
-                return true;
         }
-        return false;
-    }
-    public bool IsContainedInIncluded(List<Type> includes, Type check)
-    {
-        if (check.IsAbstract)
+        public bool IsContainedInIncluded(List<Type> includes, Type check)
+        {
+            if (check.IsAbstract)
+                return false;
+
+            for (int i = 0; i < includes.Count; i++)
+            {
+                if (includes[i].IsAssignableFrom(check))
+                    return true;
+            }
+
             return false;
-
-        for (int i = 0; i < includes.Count; i++)
-        {
-            if (includes[i].IsAssignableFrom(check))
-                return true;
         }
-
-        return false;
     }
+
 }
